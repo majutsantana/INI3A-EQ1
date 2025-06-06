cd438db3bf03ac953a56208fc2e8e4a8329edd8f
Yasmin Diniz de Oliveira <ra2357058>
2025-06-06 08:27:02 -0300

    término da tela de perfil de responsáveis e atualização no conteúdo da tela. retirada dos alunos associados

--- src/CangUp/App.tsx
+++ src/CangUp/App.tsx
@@ -1,44 +1,49 @@
-import {NavigationContainer } from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './pages/Login';
 import TipoCadastro from './pages/TipoCadastro';
 import CadastroAluno from './pages/CadastroAluno';
 import PerfilInstituicao from './pages/PerfilInstituicao';
 import PerfilAluno from './pages/PerfilAluno';
+import PerfilResponsavel from './pages/PerfilResponsavel';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   //Direciona a tela que será aberta  
-  return <NavigationContainer> 
-      <Stack.Navigator> 
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen
-            name="TipoCadastro" 
-            component={TipoCadastro}
-            options={{ headerShown: false }}
-          />
-        <Stack.Screen
-          name="CadastroAluno"
-          component={CadastroAluno}
-          options={{ headerShown: false }} // MUDAR DIRECIONAMENTO
-        />
-        <Stack.Screen
-          name="PerfilInstituicao"
-          component={PerfilInstituicao}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="PerfilAluno"
-          component={PerfilAluno}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+  return <NavigationContainer>
+    <Stack.Navigator initialRouteName='PerfilResponsavel'>
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="TipoCadastro"
+        component={TipoCadastro}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="CadastroAluno"
+        component={CadastroAluno}
+        options={{ headerShown: false }} // MUDAR DIRECIONAMENTO
+      />
+      <Stack.Screen
+        name="PerfilInstituicao"
+        component={PerfilInstituicao}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="PerfilAluno"
+        component={PerfilAluno}
+        options={{ headerShown: false }}
+      /><Stack.Screen
+        name="PerfilResponsavel"
+        component={PerfilResponsavel}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
 }
 
 // Fazer para o PerfilResponsavel depois
